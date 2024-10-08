6fb7d5ec2e16e9af9b79a9a67e8c8b391c6ac72c
nikita.mamontov <mayak1fm@gmail.com>
2024-10-08 13:51:36 +0300

    lsp

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -1,8 +1,41 @@
-local lspconfig = require('lspconfig')
-lspconfig.ccls.setup {
-  init_options = {
-    cache = {
-      directory = ".ccls-cache";
-    };
+local base = require("plugins.configs.lspconfig")
+local on_attach = base.on_attach
+local capabilities = base.capabilities
+local lspconfig = require("lspconfig")
+local wk = require('which-key')
+local diag = vim.diagnostic
+local lsp = vim.lsp
+
+wk.register(
+  {
+    l = {
+      name = 'LSP',
+      f = { function() lsp.buf.formatting() end, 'Format' },
+      i = { function() lsp.buf.implementation() end, 'Implementation' },
+      k = { function() lsp.buf.hover() end, 'Hover' },
+      l = { function() diag.open_float() end, 'Show Line Diagnostics' },
+      q = { function() diag.setloclist() end, 'Set Location List' },
+      m = { function() lsp.buf.rename() end, 'Rename' },
+      y = { function() lsp.buf.type_definition() end, 'Type Definition' },
+      -- Telescope lsp maps
+      D = { '<Cmd>Telescope lsp_document_diagnostics<Cr>', 'Diagnostics' },
+      I = { '<Cmd>Telescope lsp_implementations<Cr>', 'Implementations' },
+      d = { '<Cmd>Telescope lsp_definitions<Cr>', 'Definitions' },
+      e = { '<Cmd>Telescope lsp_code_actions<Cr>', 'Code Actions' },
+      r = { '<Cmd>Telescope lsp_references<Cr>', 'References' },
+      s = { '<Cmd>Telescope lsp_document_symbols<Cr>', 'Symbols' },
+      -- nvim-lsp-installer maps
+      L = { '<Cmd>LspInstallInfo<Cr>', 'nvim-lsp-installer UI' },
+    },
+  },
+  {
+    prefix = '<Leader>'
   }
+)
+
+lspconfig.clangd.setup{
+  on_attach = function (client,bufnr)
+   client.server_capabilities.signatureHelpProvider = false
+  end,
+  capabilities = capabilities,
 }
