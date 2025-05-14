d809e92b7facc6e06495ac7c57b07f5bc1ff0c3a
nikita.mamontov <mayak1fm@gmail.com>
2025-05-14 10:51:48 +0000

    added switch relative numbers

--- lua/options.lua
+++ lua/options.lua
@@ -4,7 +4,7 @@ require "nvchad.options"
 
 local o = vim.o
 o.cursorlineopt = 'both' -- to enable cursorline!
-o.relativenumber = true -- относительные линии!
+vim.opt.relativenumber = true -- Показывать относительные номера
 o.shell = "/usr/bin/zsh" -- терминал zsh
 o.clipboard = "unnamedplus" -- чтоб копировать по кайфу
 
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,6 +25,11 @@ return {
     },
   },
   {
+    "sitiom/nvim-numbertoggle",
+    event = "BufEnter",
+    config = true, -- автоматическая настройка
+  },
+  {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
