d20e19f16b7db27c781ff2193bff69670a34ead6
nikita.mamontov <mayak1fm@gmail.com>
2025-05-24 10:38:45 +0300

    add porotobuf lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,21 +4,29 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "clangd", "lua_ls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+lspconfig["clangd"].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+    filetypes = {"c", "cpp", "objc", "objcpp"}, -- явно указываем типы файлов
+  }
+lspconfig["lua_ls"].setup {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
   }
-end
+lspconfig["protols"].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+    filetypes = {"proto"}, -- явно указываем типы файлов
+  }
 
--- configuring single server, example: typescript
- lspconfig.clangd.setup {
-   on_attach = nvlsp.on_attach,
-   on_init = nvlsp.on_init,
-   capabilities = nvlsp.capabilities,
- }
+---- configuring single server, example: typescript
+-- lspconfig.clangd.setup {
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+-- }
