a770309b3ab8860e7af65a8dd031d8af1669f5e5
nikita.mamontov <nikita.mamontov@nami.ru>
2025-05-25 17:29:46 +0300

    add surround

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,7 +12,16 @@ return {
       require "configs.lspconfig"
     end,
   },
-
+  {
+      "kylechui/nvim-surround",
+      version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+          require("nvim-surround").setup({
+              -- Configuration here, or leave empty to use defaults
+          })
+      end
+  },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
