83ad46be647907284edfe87ea148f985a43be616
nikita.mamontov <nikita.mamontov@nami.ru>
2025-06-05 21:44:56 +0300

    add splash cursor and move cheatsheat

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,14 +13,14 @@ return {
     end,
   },
   {
-      "kylechui/nvim-surround",
-      version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
-      event = "VeryLazy",
-      config = function()
-          require("nvim-surround").setup({
-              -- Configuration here, or leave empty to use defaults
-          })
-      end
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
   },
   {
     "nvim-pack/nvim-spectre",
@@ -33,7 +33,7 @@ return {
         "vim", "lua", "vimdoc",
         "html", "css", "cpp",
         "python", "markdown",
-        "markdown_inline","cmake"
+        "markdown_inline", "cmake"
       },
     },
   },
@@ -50,19 +50,19 @@ return {
     config = function()
       require("telescope").setup({
         defaults = {
-          path_display = { "truncate" },  -- Обрезать начало пути, чтобы показать окончание
+          path_display = { "truncate" }, -- Обрезать начало пути, чтобы показать окончание
           -- Или можно использовать:
           -- path_display = { "shorten" },  -- Сократить путь, показывая только важные части
           -- path_display = { "tail" },     -- Показывать только окончание пути (имя файла)
         },
---        pickers = {
---          find_files = {
---            theme = "dropdown",  -- Использовать выпадающий список
---          },
---          live_grep = {
---            theme = "dropdown",  -- Использовать выпадающий список
---          },
---        },
+        --        pickers = {
+        --          find_files = {
+        --            theme = "dropdown",  -- Использовать выпадающий список
+        --          },
+        --          live_grep = {
+        --            theme = "dropdown",  -- Использовать выпадающий список
+        --          },
+        --        },
       })
     end,
   },
@@ -78,43 +78,43 @@ return {
   {
     "nvim-neotest/nvim-nio"
   },
- -- {
- --   "rcarriga/nvim-dap-ui",
- --   event = "VeryLazy",
- --   dependencies = "mfussenegger/nvim-dap",
- --   config = function()
- --     local dap = require("dap")
- --     local dapui = require("dapui")
- --     dapui.setup()
- --     dap.listeners.after.event_initialized["dapui_config"] = function()
- --       dapui.open()
- --     end
- --     dap.listeners.before.event_terminated["dapui_config"] = function()
- --       dapui.close()
- --     end
- --     dap.listeners.before.event_exited["dapui_config"] = function()
- --       dapui.close()
- --     end
- --   end
- -- },
- -- {
- --   "jay-babu/mason-nvim-dap.nvim",
- --   event = "VeryLazy",
- --   dependencies = {
- --     "williamboman/mason.nvim",
- --     "mfussenegger/nvim-dap",
- --   },
- --   opts = {
- --     handlers = {}
- --   },
- -- },
- -- {
- --   "mfussenegger/nvim-dap",
- --   config = function(_, _)
- --     require "configs.dapadapter"
- --     require "configs.dapconfig"
- --   end
- -- },
+  -- {
+  --   "rcarriga/nvim-dap-ui",
+  --   event = "VeryLazy",
+  --   dependencies = "mfussenegger/nvim-dap",
+  --   config = function()
+  --     local dap = require("dap")
+  --     local dapui = require("dapui")
+  --     dapui.setup()
+  --     dap.listeners.after.event_initialized["dapui_config"] = function()
+  --       dapui.open()
+  --     end
+  --     dap.listeners.before.event_terminated["dapui_config"] = function()
+  --       dapui.close()
+  --     end
+  --     dap.listeners.before.event_exited["dapui_config"] = function()
+  --       dapui.close()
+  --     end
+  --   end
+  -- },
+  -- {
+  --   "jay-babu/mason-nvim-dap.nvim",
+  --   event = "VeryLazy",
+  --   dependencies = {
+  --     "williamboman/mason.nvim",
+  --     "mfussenegger/nvim-dap",
+  --   },
+  --   opts = {
+  --     handlers = {}
+  --   },
+  -- },
+  -- {
+  --   "mfussenegger/nvim-dap",
+  --   config = function(_, _)
+  --     require "configs.dapadapter"
+  --     require "configs.dapconfig"
+  --   end
+  -- },
   { "nvim-tree/nvim-web-devicons", opts = {} },
 
   {
@@ -158,46 +158,47 @@ return {
     },
   },
   {
-   "nvim-tree/nvim-tree.lua",
-   dependencies = { "nvim-tree/nvim-web-devicons" },
-   config = function()
-     require("nvim-tree").setup({
-      view = {
-        width = "30%",  -- Ширина окна в процентах от ширины экрана
-        -- Или фиксированная ширина в пикселях:
-        -- width = 300,  -- Ширина окна в пикселях
-      },
-      renderer = {
-        indent_markers = {
-          enable = true,  -- Включить маркеры отступов
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup({
+        view = {
+          width = "30%", -- Ширина окна в процентах от ширины экрана
+          -- Или фиксированная ширина в пикселях:
+          -- width = 300,  -- Ширина окна в пикселях
         },
-      },
-     -- filters = {
-     --   dotfiles = true,  -- Показывать ли скрытые файлы (начинающиеся с точки)
-     -- },
-      actions = {
-        change_dir = {
-          enable = true,  -- Синхронизировать текущий каталог с корнем дерева
-          global = false, -- Не изменять глобальный каталог
+        renderer = {
+          indent_markers = {
+            enable = true, -- Включить маркеры отступов
+          },
         },
-        open_file = {
-          resize_window = true,  -- Автоматически изменять размер окна
-          quit_on_open = false,  -- Не закрывать дерево при открытии файла
+        -- filters = {
+        --   dotfiles = true,  -- Показывать ли скрытые файлы (начинающиеся с точки)
+        -- },
+        actions = {
+          change_dir = {
+            enable = true,  -- Синхронизировать текущий каталог с корнем дерева
+            global = false, -- Не изменять глобальный каталог
+          },
+          open_file = {
+            resize_window = true, -- Автоматически изменять размер окна
+            quit_on_open = false, -- Не закрывать дерево при открытии файла
+          },
         },
-      },
-      update_focused_file = {
-        enable = true,  -- Обновлять фокус на текущем файле
-        update_root = true,  -- Синхронизировать корень дерева с текущим каталогом
-      },
-    })
-    -- Автоматически переходить к текущему файлу при открытии nvim-tree
-    vim.api.nvim_create_autocmd("BufEnter", {
-      callback = function()
-        if vim.bo.filetype == "NvimTree" then
-          require("nvim-tree.api").tree.find_file({ open = true, focus = true })
-        end
-      end,
-    })   end,
+        update_focused_file = {
+          enable = true,      -- Обновлять фокус на текущем файле
+          update_root = true, -- Синхронизировать корень дерева с текущим каталогом
+        },
+      })
+      -- Автоматически переходить к текущему файлу при открытии nvim-tree
+      vim.api.nvim_create_autocmd("BufEnter", {
+        callback = function()
+          if vim.bo.filetype == "NvimTree" then
+            require("nvim-tree.api").tree.find_file({ open = true, focus = true })
+          end
+        end,
+      })
+    end,
   },
   {
     "olimorris/codecompanion.nvim",
@@ -237,27 +238,27 @@ return {
           changedelete = { text = "~" },
           untracked = { text = "┆" },
         },
-        signcolumn = true,     -- Toggle with `:Gitsigns toggle_signs`
-        numhl = false,         -- Toggle with `:Gitsigns toggle_numhl`
-        linehl = false,        -- Toggle with `:Gitsigns toggle_linehl`
-        word_diff = false,     -- Toggle with `:Gitsigns toggle_word_diff`
+        signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+        numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
+        linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
+        word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
         watch_gitdir = {
           interval = 1000,
           follow_files = true,
         },
         attach_to_untracked = true,
-        current_line_blame = false,     -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
         current_line_blame_opts = {
           virt_text = true,
-          virt_text_pos = "eol",       -- 'eol' | 'overlay' | 'right_align'
+          virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
           delay = 1000,
           ignore_whitespace = false,
         },
         current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
         sign_priority = 6,
         update_debounce = 100,
-        status_formatter = nil,      -- Use default
-        max_file_length = 40000,     -- Disable if file is longer than this (in lines)
+        status_formatter = nil,  -- Use default
+        max_file_length = 40000, -- Disable if file is longer than this (in lines)
         preview_config = {
           -- Options passed to nvim_open_win
           border = "single",
@@ -268,5 +269,65 @@ return {
         },
       })
     end,
+  },
+  {
+    "sphamba/smear-cursor.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- ptime_interval = 7, -- milliseconds
+      -- Smear cursor when switching buffers or windows.
+      smear_between_buffers = true,
+
+      -- Smear cursor when moving within line or to neighbor lines.
+      -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
+      smear_between_neighbor_lines = true,
+
+      -- Draw the smear in buffer space instead of screen space when scrolling
+      scroll_buffer_space = true,
+
+      -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+      -- Smears will blend better on all backgrounds.
+      legacy_computing_symbols_support = false,
+
+      -- Smear cursor in insert mode.
+      -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
+      smear_insert_mode = true,
+    },
+  },
+  {
+    'echasnovski/mini.surround',
+    event = "VeryLazy",
+    version = false
+  },
+  {
+    "tris203/precognition.nvim",
+    event = "VeryLazy",
+    opts = {
+      startVisible = true,
+      showBlankVirtLine = true,
+      highlightColor = { link = "Comment" },
+      hints = {
+        Caret = { text = "^", prio = 2 },
+        Dollar = { text = "$", prio = 1 },
+        MatchingPair = { text = "%", prio = 5 },
+        Zero = { text = "0", prio = 1 },
+        w = { text = "w", prio = 10 },
+        b = { text = "b", prio = 9 },
+        e = { text = "e", prio = 8 },
+        W = { text = "W", prio = 7 },
+        B = { text = "B", prio = 6 },
+        E = { text = "E", prio = 5 },
+      },
+      gutterHints = {
+        G = { text = "G", prio = 10 },
+        gg = { text = "gg", prio = 9 },
+        PrevParagraph = { text = "{", prio = 8 },
+        NextParagraph = { text = "}", prio = 8 },
+      },
+      disabled_fts = {
+        "startify",
+      },
+    },
   }
+
 }
