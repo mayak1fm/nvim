f6abc4287971c9f299d1afe06debb2a7cd601dd2
Nikita Mamontov <mayak1fm@gmail.com>
2025-06-08 22:40:20 +0300

    add pretyy cmd

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -297,7 +297,22 @@ return {
       { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
   },
-
+  -- lazy.nvim
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  },
   {
     "tris203/precognition.nvim",
     event = "VeryLazy",
