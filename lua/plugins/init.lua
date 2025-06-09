fd0a393f5b19d7b6f2d133744f91351ce3900fb7
nikita.mamontov <mayak1fm@gmail.com>
2025-06-09 04:55:09 +0000

    add VeryLazy in code companion

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -206,7 +206,9 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
+    event = "VeryLazy",
     config = function()
+
       require("codecompanion").setup({
         adapters = {
           deepseek = function()
