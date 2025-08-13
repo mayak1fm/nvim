bfe6ebe979a27f3b4989fe7530edfac7d575e2b0
nikita.mamontov <nikita.mamontov@nami.ru>
2025-08-13 09:17:38 +0000

    add clangd-19

--- lsp/clang.lua
+++ lsp/clang.lua
@@ -1,5 +1,18 @@
 return {
-  cmd = { 'clangd', '--background-index' },
-  root_markers = { 'compile_commands.json', 'compile_flags.txt' },
-  filetypes = { 'c', 'cpp','h','hpp','tpp' },
+ -- From the clangd configuration in <rtp>/lsp/clangd.lua
+  cmd = { 'clangd-19' },
+  -- From the clangd configuration in <rtp>/lsp/clangd.lua
+  -- Overrides the "*" configuration in init.lua
+  root_markers = { '.clangd', 'compile_commands.json' },
+  -- From the clangd configuration in init.lua
+  -- Overrides the clangd configuration in <rtp>/lsp/clangd.lua
+  filetypes = { 'c','cpp','h','hpp','tpp'},
+  -- From the "*" configuration in init.lua
+  capabilities = {
+    textDocument = {
+      semanticTokens = {
+        multilineTokenSupport = true,
+      }
+    }
+  }
 }
